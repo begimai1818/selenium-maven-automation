47cdd8a5b9e67a2dc0ac326dbcb57fc2198ab118
CybertekSchool <github@cybertekschool.com>
2018-06-26 20:59:35 -0400

    WebElement

--- src/test/java/webelements/WebElements.java
+++ src/test/java/webelements/WebElements.java
@@ -14,43 +14,46 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebElements {
-	
+
 	WebDriver driver;
-	@BeforeMethod//runs once for all tests
+
+	@BeforeMethod // runs once for all tests
 	public void setUp() {
-	WebDriverManager.chromedriver().setup();
-	driver = new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-	driver.manage().window().fullscreen();
-	
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+
 	}
-	
+
 	@Test
 	public void myLinks() {
 		driver.get("https://github.com");
 		List<WebElement> links = driver.findElements(By.tagName("a"));
-		//List<WebElement> links = driver.findElements(By.xpath("//a"));-->>xpath
-		
-		//how many links on github home page
+		// List<WebElement> links = driver.findElements(By.xpath("//a"));-->>xpath
+
+		// how many links on github home page
 		int numberOfLinksOnGithub = links.size();
 		System.out.println("number of links: " + numberOfLinksOnGithub);
-		
-		//print text of each link
-		
-		for(WebElement link : links) {
-			if(!link.getText().isEmpty())
-			System.out.println(link.getText());
-			
+
+		// print text of each link
+
+		for (WebElement link : links) {
+			if (!link.getText().isEmpty()) {
+				System.out.println(link.getText());
+
+			}
 		}
+		// add each link text into list of Strings
+		List<String> linkNames = new ArrayList<>();
+		for (WebElement link : links) {
+			if (!link.getText().isEmpty()) {
+				linkNames.add(link.getText());
+
+			}
+
+		}
+		System.out.println(linkNames.toString());
+
 	}
-	//add each link text into list of Strings
-	List<String> list = new ArrayList<>();
-	
-
-
-	
-	
-	
-	
-
 }
